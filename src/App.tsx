09cb99695c1d1454c2844a36e06c43239cc5ffc7
kunlunrepo <482976531@qq.com>
2024-05-31 10:06:42 +0800

    第一个流程

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'reactflow/dist/style.css';  // reactflow样式
+import {ReactFlow} from "reactflow";
 
 function App() {
-  const [count, setCount] = useState(0)
+    // 初始化节点
+    const initNodes = [
+        {id: '1', position: {x: 0, y: 0}, data: {label: 'Node 1'}},
+        {id: '2', position: {x: 0, y: 100}, data: {label: 'Node 2'}}
+    ]
+    // 初始化边
+    const initEdges = [
+        {id: 'e1-2', source: '1', target: '2'}
+    ]
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{width: '100vw', height: '100vh'}}>
+          <ReactFlow nodes={initNodes} edges={initEdges}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
