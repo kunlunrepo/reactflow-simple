58f383ff92d7c36d9eb53359ad428a4a20333e92
kunlunrepo <482976531@qq.com>
2024-05-31 11:48:16 +0800

    新增受控流程的连接功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
 import 'reactflow/dist/style.css';  // reactflow样式
-import ReactFlow, { applyEdgeChanges, applyNodeChanges, NodeChange, Node, Edge } from 'reactflow';
+import ReactFlow, { applyEdgeChanges, applyNodeChanges, NodeChange, Node, Edge, addEdge } from 'reactflow';
 import {useCallback, useState} from "react";
 import {initNodes} from "./flow/nodes.tsx";
 import {initEdges} from "./flow/edges.tsx";
 
 function App() {
-    // 初始化节点
-    const [nodes, setNodes] = useState<Node[]>(initNodes)
-    // 初始化边缘
+    // 初始化节点  (可以使用 const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes); 来代替)
+    const [nodes, setNodes] = useState<Node[]>(initNodes);
+    // 初始化边缘  (可以使用 const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges); 来代替)
     const [edges, setEdges] = useState<Edge[]>(initEdges);
-    // 节点变化
+    // 节点变化 (选择、拖动、删除节点时，触发这个函数，然后利用applyNodeChanges函数处理节点变化，主要是坐标等变化)
     const onNodesChange = useCallback(
-        (changes: NodeChange[]) => setNodes((nds) => applyNodeChanges(changes, nds)),
+        (changes: NodeChange[]) => {
+            console.log("节点变化 onNodesChange：", changes);
+            setNodes((nds) => applyNodeChanges(changes, nds))
+        },
         [setNodes]
     );
-    // 边缘变化
+    // 边缘变化 (删除边缘时，触发这个函数，然后利用applyEdgeChanges函数处理连接变化)
     const onEdgesChange = useCallback(
-        (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+        (changes) => {
+            console.log("边缘变化 onEdgesChange：", changes);
+            setEdges((eds) => applyEdgeChanges(changes, eds))
+        },
+        [setEdges]
+    );
+    // 连接变化 (从一个句柄连接到另一个句柄时，触发这个函数，然后利用addEdge函数处理连接关系)
+    const onConnect = useCallback(
+        (connection) => {
+            console.log("连接变化 onConnect：", connection);
+            setEdges((eds) => addEdge(connection, eds))
+        },
         [setEdges]
     );
 
@@ -24,7 +38,7 @@ function App() {
     <>
       <div style={{width: '100vw', height: '100vh'}}>
           {/*画布*/}
-          <ReactFlow nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} fitView={true}>
+          <ReactFlow nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} onConnect={onConnect} fitView={true}>
           </ReactFlow>
       </div>
     </>
