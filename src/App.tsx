3152e400105736564c904283ef02910513ef41d8
kunlunrepo <482976531@qq.com>
2024-05-31 18:31:16 +0800

    开发工具

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,60 @@
 import 'reactflow/dist/style.css';  // reactflow样式
 import {
-    ReactFlow,
+    ReactFlow, Edge, Node, useNodesState, Connection, addEdge, useEdgesState
 } from "reactflow";
-import {useShallow} from 'zustand/react/shallow';
-import useStore from './flow/store';
-import ColorChooserNode from './flow/ColorChooserNode';
 
-const nodeTypes = { colorChooser: ColorChooserNode };
+import DevTools from './devtools/Devtools';
+import './devtools/style.css';
+import {useCallback} from "react";
 
-const selector = (state: any) => ({
-    nodes: state.nodes,
-    edges: state.edges,
-    onNodesChange: state.onNodesChange,
-    onEdgesChange: state.onEdgesChange,
-    onConnect: state.onConnect,
-});
+const initNodes: Node[] = [
+    {
+        id: '1a',
+        type: 'input',
+        data: { label: 'Node 1' },
+        position: { x: 250, y: 5 },
+    },
+    {
+        id: '2a',
+        data: { label: 'Node 2' },
+        position: { x: 100, y: 120 },
+    },
+    {
+        id: '3a',
+        data: { label: 'Node 3' },
+        position: { x: 400, y: 120 },
+    },
+];
+
+const initEdges: Edge[] = [
+    { id: 'e1-2', source: '1a', target: '2a' },
+    { id: 'e1-3', source: '1a', target: '3a' },
+];
+
+const fitViewOptions = { padding: 0.5 };
 
 function App() {
-    // 获取store
-    const { nodes, edges, onNodesChange, onEdgesChange, onConnect } = useStore(
-        useShallow(selector),
+    const [nodes, , onNodesChange] = useNodesState(initNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initEdges);
+
+    const onConnect = useCallback(
+        (params: Connection | Edge) => setEdges((eds) => addEdge(params, eds)),
+        [setEdges],
     );
 
     return (
         <div style={{ height: '100vh', width: '100vw'}}>
             <ReactFlow
-            nodes={nodes}
-            edges={edges}
-            onNodesChange={onNodesChange}
-            onEdgesChange={onEdgesChange}
-            onConnect={onConnect}
-            nodeTypes={nodeTypes}
-            fitView
-            />
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                fitView
+                fitViewOptions={fitViewOptions}
+            >
+                <DevTools />
+            </ReactFlow>
         </div>
     );
 }
